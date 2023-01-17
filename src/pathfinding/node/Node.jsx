aac861ec3053d2e461438c57bb806de6c17ded53
Zsoltbanyai <zsolt.banyai94@gmail.com>
2023-01-17 17:24:27 +0100

    update package.json reformat code to supplement it

--- src/pathfinding/node/Node.jsx
+++ src/pathfinding/node/Node.jsx
@@ -1,5 +1,9 @@
 import './Node.css'
 
-export const Node = ({row, col}) => {
-    return <div className="node"></div>
+export const Node = ({row, col, isStart, isEnd}) => {
+    let className = 'node'
+    if (isStart) className += ' start-node'
+    if (isEnd) className += ' end-node'
+    return <div className={className}></div>
 }
+
